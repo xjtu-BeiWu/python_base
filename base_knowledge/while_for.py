5bd61fde27f0bd4aac0e431aeffc4e2ceafce9b4
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-01-19 22:06:12 +0800

    while-for

--- base_knowledge/while_for.py
+++ base_knowledge/while_for.py
@@ -6,3 +6,23 @@ while condition < 10:
 example_list = [0,1,2,3,4,5,6,7,8,9,10]
 for i in example_list:
     print(i)
+    print("inner of for")
+print("outer of for")
+
+# for tuple
+tup = ('python', 2.7, 64)
+for i in tup:
+    print(i)
+
+# for dictionary
+dic = {}
+dic['lan'] = 'python'
+dic['version'] = 2.7
+dic['platform'] = 64
+for key in dic:
+    print(key, dic[key])
+
+# for set
+s = set(['python', 'python2', 'python3','python'])
+for item in s:
+    print(item)
