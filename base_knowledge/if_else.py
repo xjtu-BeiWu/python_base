5b4649a4618634e467dc217e5ee5a188a3c89f63
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-08 20:53:04 +0800

    if else

---
+++ base_knowledge/if_else.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+x = 1
+y = 2
+z = 0
+if x > y:
+    print("x is greater than y.")
+else:
+    print("x is less than y.")
+
+worked = False
+result = 'done' if worked else 'not yet'
+print(result)
