b04cc31006a738a56ce4a099341acf2415cb6e12
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-08 20:56:56 +0800

    if elif else

--- base_knowledge/if_else.py
+++ base_knowledge/if_else.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-x = 1
+x = 3
 y = 2
 z = 0
 if x > y:
@@ -9,5 +9,13 @@ else:
     print("x is less than y.")
 
 worked = False
+# 通过行内表达式完成类似三目操作符
 result = 'done' if worked else 'not yet'
 print(result)
+
+if x > 1:
+    print('x>1')
+elif x < 1:
+    print('x<1')
+else:
+    print('x=1')
