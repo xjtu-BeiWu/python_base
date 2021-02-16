71a2948c81f9131edc7e88793b721eff95a2172c
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-16 21:07:13 +0800

    modified

--- base_knowledge/read_write_file.py
+++ base_knowledge/read_write_file.py
@@ -4,12 +4,19 @@
 # File: read_write_file.py
 # IDE: PyCharm
 
-def read(file_path):
+def read_write(file_path):
     append_text = '\nThis is appended line.'
     my_file = open(file_path, 'a+')
     my_file.write(append_text)
     my_file.close()
 
+def read(file_path):
+    file = open(file_path,'r')
+    content = file.readlines()
+    print(content)
+    file.close()
+
 file_path = '/test_file/my_file.txt'
+read_write(file_path)
 read(file_path)
 
