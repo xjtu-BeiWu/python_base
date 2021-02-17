3a92ca10ccf333644e2bab67d6e866d896730936
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-17 10:14:57 +0800

    about datatype

--- base_knowledge/data_type.py
+++ base_knowledge/data_type.py
@@ -32,3 +32,20 @@ a_list.sort()
 print(a_list)
 a_list.sort(reverse=True)
 print(a_list)
+
+# 2-dimension list
+multi_dim_list = [[1,2,3],
+                  [2,3,4],
+                  [3,4,5]]
+print(multi_dim_list[0][1])
+
+# dictionary:key value
+# 在同一字典中并不需要所有的key和value有相同的形式
+d1 = {'apple':1, 'pear':2, 'orange':3}
+d2 = {1:'a', 2:'b', 3:'c'}
+d3 = {1:'a', 'b':2, 'c':3}
+print(d1['apple'])
+del d1['apple']
+print(d1)
+d1['b'] = 20
+print(d1) # 字典元素并不会按照规律打印出来，字典是一个无序的容器。
