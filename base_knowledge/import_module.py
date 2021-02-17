e7fd4fe4c80a854062dba05da578191ec6836c56
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-17 10:50:19 +0800

    about import module

---
+++ base_knowledge/import_module.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-  
+# Author: bellawu
+# Date: 2021/2/17 10:17
+# File: import_module.py
+# IDE: PyCharm
+
+# import time
+#
+# print(time.localtime())
+
+# import time as t
+# print(t.localtime())
+
+# 仅输入自己需要的功能
+# from time import time, localtime
+# print(time())
+# print(localtime())
+
+# 输入模块所有功能
+from time import *
+print(localtime())
+
