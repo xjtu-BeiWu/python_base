cccea8d546afb46719ec63c41a00925d941a3350
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-02-22 10:29:35 +0800

    update

--- base_knowledge/multiprocessing_pool.py
+++ base_knowledge/multiprocessing_pool.py
@@ -25,24 +25,4 @@ def multi_process():
 
 
 if __name__ == '__main__':
-    # multi_process()
-    # shared memory
-    value = multiprocessing.Value('i', 0)
-    # 这里的Array和numpy不一样，只能是一维的，不能是多维的
-    array = multiprocessing.Array('i', [1, 2, 3])
-
-# | Type code | C Type             | Python Type       | Minimum size in bytes |
-# | --------- | ------------------ | ----------------- | --------------------- |
-# | `'b'`     | signed char        | int               | 1                     |
-# | `'B'`     | unsigned char      | int               | 1                     |
-# | `'u'`     | Py_UNICODE         | Unicode character | 2                     |
-# | `'h'`     | signed short       | int               | 2                     |
-# | `'H'`     | unsigned short     | int               | 2                     |
-# | `'i'`     | signed int         | int               | 2                     |
-# | `'I'`     | unsigned int       | int               | 2                     |
-# | `'l'`     | signed long        | int               | 4                     |
-# | `'L'`     | unsigned long      | int               | 4                     |
-# | `'q'`     | signed long long   | int               | 8                     |
-# | `'Q'`     | unsigned long long | int               | 8                     |
-# | `'f'`     | float              | float             | 4                     |
-# | `'d'`     | double             | float             | 8                     |
+    multi_process()
