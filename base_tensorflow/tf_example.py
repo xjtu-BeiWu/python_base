20d139c938ae4ac9615dd9206fdac15a2b9ce5b8
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-03-07 21:50:54 +0800

    first tensorflow example

---
+++ base_tensorflow/tf_example.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-  
+# Author: bellawu
+# Date: 2021/3/3 21:50
+# File: tf_example.py
+# IDE: PyCharm
+
+import tensorflow as tf
+import numpy as np
+
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights * x_data + biases
+
+loss = tf.reduce_mean(tf.square(y-y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+sess = tf.Session()
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+    init = tf.initialize_all_variables()
+else:
+    init = tf.global_variables_initializer()
+sess.run(init)
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(biases))
