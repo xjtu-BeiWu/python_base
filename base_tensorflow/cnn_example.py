28000edde8056b645f24f35e13ddf4dc0c83a70e
xjtu-BeiWu <xjtu_BeiWu@163.com>
2021-03-15 20:36:25 +0800

    add name

--- base_tensorflow/cnn_example.py
+++ base_tensorflow/cnn_example.py
@@ -19,54 +19,54 @@ def compute_accuracy_prob(value_x, value_y):
     return result
 
 
-def weight_variable(shape):
+def weight_variable(shape, name_str):
     # truncated_normal 截断的产生正态分布的随机数，即随机数与均值的差值若大于两倍的标准差，则重新生成。
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name_str)
 
 
-def bias_variable(shape):
+def bias_variable(shape, name_str):
     initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+    return tf.Variable(initial, name=name_str)
 
 
-def conv2d(x, w):
-    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, w, name_str):
+    return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME', name=name_str)
 
 
-def max_pool_2x2(x):
+def max_pool_2x2(x, name_str):
     # return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')  # 0.969
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # 0.969
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name_str)  # 0.969
 
 
-keep_prob = tf.placeholder(tf.float32)
-x_input = tf.placeholder(tf.float32, [None, 784])
-y_input = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+x_input = tf.placeholder(tf.float32, [None, 784], name='x_inputs')
+y_input = tf.placeholder(tf.float32, [None, 10], name='y_inputs')
 
-x_image = tf.reshape(x_input, [-1, 28, 28, 1])
+x_image = tf.reshape(x_input, [-1, 28, 28, 1], name='x_image')
 
 # 构建第一层卷积
 # 卷积核大小为5*5，32个feature map
-w_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1)+b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+w_conv1 = weight_variable([5, 5, 1, 32], 'w1')
+b_conv1 = bias_variable([32], 'b1')
+h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1, 'conv1')+b_conv1)
+h_pool1 = max_pool_2x2(h_conv1, 'pool1')
 
 # 构建第二层卷积
-w_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2)+b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+w_conv2 = weight_variable([5, 5, 32, 64], 'w2')
+b_conv2 = bias_variable([64], 'b2')
+h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2, 'conv2')+b_conv2)
+h_pool2 = max_pool_2x2(h_conv2, 'pool2')
 
 # 构建全连接层
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-w_fc1 = weight_variable([7*7*64, 1024])
-b_fc1 = bias_variable([1024])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1)+b_fc1)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64], name='h_pool2_flat')
+w_fc1 = weight_variable([7*7*64, 1024], 'w_fc1')
+b_fc1 = bias_variable([1024], 'b_fc1')
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1)+b_fc1, name='h_fc1')
 # 加入dropout
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-w_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob, name='h_fc1_drop')
+w_fc2 = weight_variable([1024, 10], 'w_fc2')
+b_fc2 = bias_variable([10], 'b_fc2')
 prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2)+b_fc2)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_input*tf.log(prediction), reduction_indices=[1]))
@@ -83,6 +83,6 @@ for i in range(1000):
     if i % 50 == 0:
         print(compute_accuracy_prob(mnist.test.images[:1000], mnist.test.labels[:1000]))
 
-save_path = saver.save(sess, '../my_net/save_net_cnn.ckpt')
+save_path = saver.save(sess, '../my_net/save_net_cnn_name.ckpt')
 print('Save to path: ', save_path)
 print('Everything is finished!')
